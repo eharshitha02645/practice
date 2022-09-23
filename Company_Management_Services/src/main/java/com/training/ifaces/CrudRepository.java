b0192221f1fc1401df833dbb58b6b6d6be5101d0
AD18780 <AD18780@IND-5CG2216WT3.ctl.intranet>
2022-09-23 17:00:26 +0530

    Core Java Project

--- Company_Management_Services/src/main/java/com/training/ifaces/CrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.training.ifaces;
-import java.time.LocalDate;
-import java.util.Collection;
-import com.training.exceptions.ElementNotFoundException;
-public interface CrudRepository<T> {
-	
-	public boolean save(T obj);
-	public Collection<T> findAll();
-}
